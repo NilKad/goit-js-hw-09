fbb2ef46325f0af224e0c410ad5e44477984e2f0
NilKad <kadulin@gmail.com>
2022-09-25 20:20:54 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const bodyStyle = document.body.style;
+btnStop.disabled = true;
 
+let timerId = null;
+
+const getRandomHexColor = () =>
+  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const changeBackgroundColor = () =>
+  (bodyStyle.backgroundColor = getRandomHexColor());
+
+const toggleBtn = () => {
+  btnStart.toggleAttribute('disabled');
+  btnStop.toggleAttribute('disabled');
+};
+
+const startTimer = evt => {
+  changeBackgroundColor();
+  toggleBtn();
+  timerId = setInterval(changeBackgroundColor, 1000);
+};
+
+const stopTimer = evt => {
+  clearInterval(timerId);
+  toggleBtn();
+};
+
+const onStartBtn = btnStart.addEventListener('click', startTimer);
+const onStopBtn = btnStop.addEventListener('click', stopTimer);
