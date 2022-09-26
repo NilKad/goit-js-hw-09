4a2c4d5b4f37bdf975337e471802abca65404886
NilKad <kadulin@gmail.com>
2022-09-26 17:46:28 +0300

    task-2

---
+++ src/js/02-timer.js
@@ -0,0 +1,113 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min';
+
+const notify = Notiflix.Notify;
+
+const dateInputRef = document.querySelector('#datetime-picker');
+const startBtnRef = document.querySelector('[data-start]');
+const displayDayRef = document.querySelector('[data-days]');
+const displayHourRef = document.querySelector('[data-hours]');
+const displayMinRef = document.querySelector('[data-minutes]');
+const displaySecRef = document.querySelector('[data-seconds]');
+startBtnRef.disabled = true;
+let dateEnd = null;
+let timerEnd = null;
+let isStartTimer = false;
+let timerId = null;
+
+const optionsFlatpickr = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    changeSelectedDate(selectedDates[0]);
+    // console.log((selectedDates[0] - Date.now()) / 1000);
+  },
+  onChange(selectedDates) {
+    changeSelectedDate(selectedDates[0]);
+    // console.log('Date selected', selectedDates[0]);
+  },
+};
+
+const onClickBtnStart = e => {
+  if (!changeSelectedDate(dateEnd)) {
+    return false;
+  }
+  if (isStartTimer) {
+    clearInterval(timerId);
+    isStartTimer = false;
+    // return;
+  }
+  const getEndDateObject = dateToObject(dateEnd - Date.now());
+  startTimer(dateEnd);
+};
+
+const changeSelectedDate = selectedDates => {
+  const dataCheck = checkInputDateVsCurrent(selectedDates);
+  isBtnStartDisabled(!dataCheck);
+  if (!dataCheck) {
+    notify.failure('Please choose a date in the future');
+  }
+  return dataCheck;
+};
+
+const isBtnStartDisabled = e => {
+  startBtnRef.disabled = e;
+};
+
+const addLeadingZero = num => String(num).padStart(2, '0');
+
+const checkInputDateVsCurrent = selectedDates => {
+  dateEnd = selectedDates;
+  if (Date.now() < selectedDates) {
+    return true;
+  }
+  return false;
+};
+
+const dateToObject = dateInput => {
+  // dateInput = 4000000;
+  // dateInput = 86344626;
+  // dateInput = 171258626;
+  // dateInput = 343861626;
+  dateInput = Math.floor(dateInput / 1000);
+
+  const sec = dateInput % 60;
+  const min = ((dateInput - sec) / 60) % 60;
+  const hour = (((dateInput - sec) / 60 - min) / 60) % 24;
+  const day = (((dateInput - sec) / 60 - min) / 60 - hour) / 24;
+  // console.log(`day: ${day} hour: ${hour} min: ${min} sec: ${sec}`);
+  return { day, hour, min, sec };
+};
+
+const renderTimer = ({ day, hour, min, sec }) => {
+  // console.log(sec);
+  displayDayRef.textContent = addLeadingZero(day);
+  displayHourRef.textContent = addLeadingZero(hour);
+  displayMinRef.textContent = addLeadingZero(min);
+  displaySecRef.textContent = addLeadingZero(sec);
+};
+
+const startTimer = dateEnd => {
+  timerEnd = dateEnd;
+  notify.info('Timer started');
+
+  timerId = setInterval(() => {
+    timerCount = timerEnd - Date.now();
+    isStartTimer = true;
+    if (timerCount <= 0) {
+      clearInterval(timerId);
+      notify.success('Time end');
+      return;
+    }
+    renderTimer(dateToObject(timerCount));
+  }, 1000);
+};
+
+const stopTimer = () => {};
+
+const dateOut = new flatpickr(dateInputRef, optionsFlatpickr);
+startBtnRef.addEventListener('click', onClickBtnStart);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,10 @@
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min';
+
+const form = document.querySelector('.form');
+
+const notify = Notiflix.Notify;
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
